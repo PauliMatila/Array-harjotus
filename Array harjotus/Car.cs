151a47958015ecce45729505450155c08ff3a356
Pauli Matila <pauli.matila@outlook.com>
2020-04-02 14:43:47 +0300

    Array tehtävä alotettu.

---
+++ Array harjotus/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array_harjotus
+{
+    public class Car
+    {
+        public string brand;
+        public int kilometers;
+        public bool taxPaid;
+
+        public Car()
+        {
+
+        }
+        
+        public Car(string brand, int kilometers)
+        {
+            this.brand = brand;
+            this.kilometers = kilometers;
+        }
+
+        public void AddKilometers()
+        {
+            kilometers += 3;
+        }
+
+        public void PrintCarData()
+        {
+            Console.WriteLine($"Auton merkki on: {brand}.");
+            Console.WriteLine($"Autolla on ajettu: {kilometers}.");
+            Console.WriteLine($"Onko auton verot maksettu?: {taxPaid}");
+        }
+
+        
+    }
+}
--- Array harjotus/Program.cs
+++ Array harjotus/Program.cs
@@ -6,7 +6,40 @@ namespace Array_harjotus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car auto1 = new Car();
+            auto1.brand = "BMW";
+            auto1.kilometers = 30100;
+            auto1.taxPaid = true;
+            
+            Car auto2 = new Car();
+            auto2.brand = "Toyota";
+            auto2.kilometers = 177000;
+            auto2.taxPaid = false;
+            
+            Car auto3 = new Car();
+            auto3.brand = "Audi";
+            auto3.kilometers = 123000;
+            auto3.taxPaid = false;
+            
+            Car auto4 = new Car();
+            auto4.brand = "Honda";
+            auto4.kilometers = 87000;
+            auto4.taxPaid = true;
+            
+            Car auto5 = new Car();
+            auto5.brand = "Volkswagen";
+            auto5.kilometers = 485000;
+            auto5.taxPaid = false;
+
+            Car[] carsOne = new Car[3] {auto1, auto3, auto5};
+            foreach (Car carsArrayOne in carsOne)
+            {
+                Console.WriteLine(carsArrayOne.brand);
+                Console.WriteLine(carsArrayOne.kilometers);
+            }
+
+            Car[] carsTwo = { auto2, auto3, auto4 };
+            
         }
     }
 }
